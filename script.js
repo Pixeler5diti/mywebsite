cac28d9eea30fee27a42fba90e8f74ff67543201
Diti Vasisht <ditivasisht@gmail.com>
2024-12-02 20:03:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      if (this.hash !== "") {
+        e.preventDefault();
+        const hash = this.hash;
+        document.querySelector(hash).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
+  
